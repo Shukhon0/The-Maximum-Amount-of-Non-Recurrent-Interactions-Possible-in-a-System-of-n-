bd56fded9d01f73fab862c915c68c2987b8060bb
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2021-07-10 14:25:59 +0600

    Create maxinteraction.js
    change the variable 'number' to change n [here, number is n]

---
+++ maxinteraction.js
@@ -0,0 +1,8 @@
+var number = (6);
+// don't shake hands during a pandemic lmao
+// no really don't
+function maxint () {
+    return number*(number-1)/2; 
+}
+document.getElementById('nth').textContent=(number);
+document.getElementById('sum').textContent=(maxint());
